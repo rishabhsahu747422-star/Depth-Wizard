b3cc573aa633651383405e1e2eb9559f0129216d
rishabhsahu747422-star <rishabhsahu747422@gmail.com>
2026-09-07 19:27:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="font-semibold text-4xl">Hello from Depth Wizard</h1>
+    </div>
+  );
+};
+
+export default App;
